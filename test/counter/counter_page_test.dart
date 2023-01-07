31092bae170f68fe3f6f99af69666242f4cf457a
Hiroto Kitamura <kirikonknow@gmail.com>
2023-01-07 17:10:59 +0900

    refactor test.

--- test/counter/counter_page_test.dart
+++ test/counter/counter_page_test.dart
@@ -7,15 +7,7 @@ void main() {
   testWidgets('''
     Counter page has a title, message, and increment button
   ''', (tester) async {
-    await tester.pumpWidget(
-      // ProviderScope is required to use Riverpod
-      const ProviderScope(
-        // MaterialApp is required to use Scaffold widgets
-        child: MaterialApp(
-          home: CounterPage(),
-        ),
-      ),
-    );
+    await tester.pumpWidget(_buildTestWidget());
 
     const expectTitle = 'Counter with Riverpod';
     const expectMessage = 'You have pushed the button this many times:';
@@ -28,9 +20,7 @@ void main() {
   testWidgets('''
     Counter initial value is 0
   ''', (tester) async {
-    await tester.pumpWidget(
-      const ProviderScope(child: MaterialApp(home: CounterPage())),
-    );
+    await tester.pumpWidget(_buildTestWidget());
 
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
@@ -39,9 +29,7 @@ void main() {
   testWidgets('''
     If the increment button is pressed, the counter value is incremented
   ''', (tester) async {
-    await tester.pumpWidget(
-      const ProviderScope(child: MaterialApp(home: CounterPage())),
-    );
+    await tester.pumpWidget(_buildTestWidget());
 
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
@@ -55,4 +43,14 @@ void main() {
     expect(find.text('1'), findsNothing);
     expect(find.text('2'), findsOneWidget);
   });
+}
+
+Widget _buildTestWidget() {
+  // ProviderScope is required to use Riverpod
+  return const ProviderScope(
+    // MaterialApp is required to use Scaffold widgets
+    child: MaterialApp(
+      home: CounterPage(),
+    ),
+  );
 }
