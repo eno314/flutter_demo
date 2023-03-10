1fc9669049810c41f15181c152c73da70224bbb7
Hiroto Kitamura <kirikonknow@gmail.com>
2023-03-11 07:42:35 +0900

    remove unused import

--- lib/chat_gpt/chat_gpt_page.dart
+++ lib/chat_gpt/chat_gpt_page.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/chat_gpt/chat_gpt_client.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
